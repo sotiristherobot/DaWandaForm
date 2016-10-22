f7ece23166a67c0e3a4b2185b41a015ab3d7a2b3
Sotiris Ioannou <me@sotiris.engineer>
2016-10-22 09:46:48 -0400

    Validate form for blank inputs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,17 +9,43 @@ $(document).ready(function () {
     var email = $("#email").val();
     var pass = $("#password").val();
 
-    //place everything into an array
+    //place everything into an object
+    var inputObject = {
+      name : name,
+      lastName : lastName,
+      nickName : nickName,
+      email : email,
+      pass : pass
+    };
 
+    //check if there are any blank fields
+    validateBlank(inputObject);
 
   });
 
 
   //check if input boxes are blank
-  function validateBlank() {
+  function validateBlank(inputObject) {
 
+    if (inputObject.name == ""){
+      console.log("name cannot be blank");
+    }
 
+    if (inputObject.lastName == ""){
+      console.log("lastname cannot be blank");
+    }
 
+    if (inputObject.nickName == ""){
+      console.log("nickName cannot be blank");
+    }
+
+    if (inputObject.email == ""){
+      console.log("email cannot be blank");
+    }
+
+    if (inputObject.pass == ""){
+      console.log("pass cannot be blank");
+    }
 
 
   }

899e93f3d7c8362c1e097ecbcdb2e1aa0dee66f6
Sotiris Ioannou <me@sotiris.engineer>
2016-10-22 09:36:47 -0400

    Get values from the form

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,27 @@
 $(document).ready(function () {
 
-  $("#submit").click(function () {console.log("test")});
+  //event listener for submit buttons clicks
+  $("#submit").click(function () {
+    //get values from form
+    var name = $("#name").val();
+    var lastName = $("#lastname").val();
+    var nickName = $("#nickname").val();
+    var email = $("#email").val();
+    var pass = $("#password").val();
 
+    //place everything into an array
+
+
+  });
+
+
+  //check if input boxes are blank
+  function validateBlank() {
+
+
+
+
+
+  }
 
 });
